c7001ae018f26aa7e2bbe6ee7434b0c536ac53b0
LucasWermann <lucasvw.eng@gmail.com>
2023-06-05 11:11:06 -0300

    Componente: TextInput

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,12 +1,22 @@
-import { Text, View } from 'react-native'
+import { Text, TextInput, View } from 'react-native'
 import { styles } from './styles'
 
-export default function Home() {
+export function Home() {
   return (
-    <View>
-      <Text>
-        Home
+    <View style={styles.container}>
+      <Text style={styles.eventName}>
+        Nome do evento
       </Text>
+
+      <Text style={styles.eventDate}>
+        Sexta, 4 de novembro de 2022
+      </Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder='Nome do participante'
+        placeholderTextColor={'#6B6B6B'}
+      />
     </View>
   )
 }
