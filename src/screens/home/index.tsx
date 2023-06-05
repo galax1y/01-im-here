9faee0eaf33ff137c42159c22b5846470208c0f5
LucasWermann <lucasvw.eng@gmail.com>
2023-06-05 11:35:08 -0300

    Componente TouchableOpacity e estrutura do Form

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,7 +1,13 @@
-import { Text, TextInput, View } from 'react-native'
+import { Text, TextInput, TouchableOpacity, View } from 'react-native'
 import { styles } from './styles'
+import { useState } from 'react'
+
 
 export function Home() {
+  function handleNewParticipant() {
+
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>
@@ -12,11 +18,32 @@ export function Home() {
         Sexta, 4 de novembro de 2022
       </Text>
 
-      <TextInput
-        style={styles.input}
-        placeholder='Nome do participante'
-        placeholderTextColor={'#6B6B6B'}
-      />
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder='Nome do participante'
+          placeholderTextColor={'#6B6B6B'}
+        />
+        <TouchableOpacity
+          style={styles.button}
+          onPress={handleNewParticipant}
+        >
+          <Text style={styles.buttonText}>
+            +
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+
+      {/* <View>
+        {participants.map((participant) => {
+          return (
+            <Text key={participant}>
+              {participant}
+            </Text>
+          )
+        })}
+      </View> */}
     </View>
   )
 }
